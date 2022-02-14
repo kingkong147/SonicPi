9d8395b2f17656d593ab8cec6fe7efa8a1397172
kingkong147 <99356442+kingkong147@users.noreply.github.com>
2022-02-14 11:39:10 -0800

    Update ChallengeC.rb

--- ChallengeC.rb
+++ ChallengeC.rb
@@ -1,1 +1,38 @@
+use_bpm 160
+use_synth :tri
+define :stranger_things_theme do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+with_fx :wobble do
+  stranger_things_theme
+end
 
+
+with_fx :whammy do
+  stranger_things_theme
+end
+
+
+with_fx :slicer do
+  stranger_things_theme
+end
+
+
+with_fx :octaver do
+  stranger_things_theme
+end
